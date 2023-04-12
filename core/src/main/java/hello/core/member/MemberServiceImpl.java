96988ccdeb1587d7eab2025b07b997f7ae2927b2
GEONEUI <dbrjsdml@naver.com>
2023-04-12 20:10:26 +0900

    스프링으로 전환하기

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -4,7 +4,6 @@ public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository;
 
-
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
